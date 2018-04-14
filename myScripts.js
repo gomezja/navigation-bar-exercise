ce3a6015ffa99c60772ac7686cb52383ce2e478e
gomezja <jgomez42gb@gmail.com>
2018-04-13 17:39:45 -0700

    made dropdown item clickable

--- myScripts.js
+++ myScripts.js
@@ -1,10 +1,21 @@
-// gets called when user preses hamburger icon
+// nav icon is pressed
 function responsiveManager() {
     var nav = document.getElementById("nav");
-
+    
     if (nav.className === "navbar") {
         nav.className += " responsive";
     } else {
         nav.className = "navbar";
+    }
+}
+
+// dropdown nav item is clicked
+function dropdownClicked() {
+    var drop = document.getElementById("dropdown");
+
+    if (drop.className === "dropdown-content") {
+        drop.className = "dropdown-content clicked";
+    } else {
+        drop.className = "dropdown-content";
     }
 }
